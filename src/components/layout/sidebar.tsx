c8107290121bc3d9206eb7a267da02f30c2bf7a8
Asher <as.asher.anjum@gmail.com>
2025-02-10 09:35:48 +0400

    refactor: move settings link to user dropdown menu

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -69,11 +69,6 @@ const navItems = [
     title: "Accounting",
     icon: <DollarSign className="h-4 w-4" />,
   },
-  {
-    href: "/settings",
-    title: "Settings",
-    icon: <Settings className="h-4 w-4" />,
-  },
 ];
 
 export function AppSidebar() {
